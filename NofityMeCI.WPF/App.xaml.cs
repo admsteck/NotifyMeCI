b8a4163a97e65aaff749f526193126282e179b32
Stechschulte, Adam M <astechschulte@paychex.com>
2016-06-23 15:01:57 -0400

    wip

---
+++ NofityMeCI.WPF/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace NofityMeCI.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        private NotifyIcon _icon;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ShutdownMode = ShutdownMode.OnExplicitShutdown;
+            InitIcon();
+        }
+
+        private void InitIcon()
+        {
+            _icon = new NotifyIcon
+            {
+                Icon = new System.Drawing.Icon("Resources\\green_icon.ico"),
+                ContextMenu = InitContextMenu(),
+                Visible = true
+            };
+            _icon.DoubleClick += ShowWindow;
+        }
+
+        private ContextMenu InitContextMenu()
+        {
+            var cm = new ContextMenu();
+            cm.MenuItems.Add("Exit", ExitClick);
+            return cm;
+        }
+
+        private void ShowWindow(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExitClick(object sender, EventArgs e)
+        {
+            _icon.Visible = false;
+            Current.Shutdown();
+        }
+    }
+}
